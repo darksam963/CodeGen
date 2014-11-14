6e1eed94d110932b48ffc0c6b0320a029113c953
darksam963 <cs12b1035@iith.ac.in>
2014-11-14 06:30:40 -0800

    Setup

---
+++ test/test.s
@@ -0,0 +1,406 @@
+# start of generated code
+	.data
+	.align	2
+	.globl	class_nameTab
+	.globl	Main_protObj
+	.globl	Int_protObj
+	.globl	String_protObj
+	.globl	bool_const0
+	.globl	bool_const1
+	.globl	_int_tag
+	.globl	_bool_tag
+	.globl	_string_tag
+_int_tag:
+	.word	2
+_bool_tag:
+	.word	3
+_string_tag:
+	.word	4
+	.globl	_MemMgr_INITIALIZER
+_MemMgr_INITIALIZER:
+	.word	_NoGC_Init
+	.globl	_MemMgr_COLLECTOR
+_MemMgr_COLLECTOR:
+	.word	_NoGC_Collect
+	.globl	_MemMgr_TEST
+_MemMgr_TEST:
+	.word	0
+	.word	-1
+str_const17:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	int_const6
+	.byte	0	
+	.align	2
+	.word	-1
+str_const16:
+	.word	4
+	.word	6
+	.word	String_dispTab
+	.word	int_const2
+	.ascii	"Main"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const15:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	int_const4
+	.ascii	"A"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const14:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	int_const4
+	.ascii	"B"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const13:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	int_const4
+	.ascii	"C"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const12:
+	.word	4
+	.word	6
+	.word	String_dispTab
+	.word	int_const7
+	.ascii	"String"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const11:
+	.word	4
+	.word	6
+	.word	String_dispTab
+	.word	int_const2
+	.ascii	"Bool"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const10:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	int_const1
+	.ascii	"Int"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const9:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	int_const3
+	.ascii	"IO"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const8:
+	.word	4
+	.word	6
+	.word	String_dispTab
+	.word	int_const7
+	.ascii	"Object"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const7:
+	.word	4
+	.word	7
+	.word	String_dispTab
+	.word	int_const8
+	.ascii	"_prim_slot"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const6:
+	.word	4
+	.word	7
+	.word	String_dispTab
+	.word	int_const9
+	.ascii	"SELF_TYPE"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const5:
+	.word	4
+	.word	7
+	.word	String_dispTab
+	.word	int_const9
+	.ascii	"_no_class"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const4:
+	.word	4
+	.word	8
+	.word	String_dispTab
+	.word	int_const10
+	.ascii	"<basic class>"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const3:
+	.word	4
+	.word	7
+	.word	String_dispTab
+	.word	int_const9
+	.ascii	"test this"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const2:
+	.word	4
+	.word	6
+	.word	String_dispTab
+	.word	int_const0
+	.ascii	"done\n"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const1:
+	.word	4
+	.word	6
+	.word	String_dispTab
+	.word	int_const0
+	.ascii	"hello"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const0:
+	.word	4
+	.word	8
+	.word	String_dispTab
+	.word	int_const11
+	.ascii	"test/test.cl"
+	.byte	0	
+	.align	2
+	.word	-1
+int_const11:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	12
+	.word	-1
+int_const10:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	13
+	.word	-1
+int_const9:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	9
+	.word	-1
+int_const8:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	10
+	.word	-1
+int_const7:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	6
+	.word	-1
+int_const6:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	0
+	.word	-1
+int_const5:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	67
+	.word	-1
+int_const4:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	1
+	.word	-1
+int_const3:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	2
+	.word	-1
+int_const2:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	4
+	.word	-1
+int_const1:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	3
+	.word	-1
+int_const0:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	5
+	.word	-1
+bool_const0:
+	.word	3
+	.word	4
+	.word	Bool_dispTab
+	.word	0
+	.word	-1
+bool_const1:
+	.word	3
+	.word	4
+	.word	Bool_dispTab
+	.word	1
+class_nameTab:
+	.word	str_const8
+	.word	str_const9
+	.word	str_const10
+	.word	str_const11
+	.word	str_const12
+	.word	str_const13
+	.word	str_const14
+	.word	str_const15
+	.word	str_const16
+class_objTab:
+	.word	Object_protObj
+	.word	Object_init
+	.word	IO_protObj
+	.word	IO_init
+	.word	Int_protObj
+	.word	Int_init
+	.word	Bool_protObj
+	.word	Bool_init
+	.word	String_protObj
+	.word	String_init
+	.word	C_protObj
+	.word	C_init
+	.word	B_protObj
+	.word	B_init
+	.word	A_protObj
+	.word	A_init
+	.word	Main_protObj
+	.word	Main_init
+Main_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	Main.main
+A_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	A.f2
+	.word	B.g
+	.word	A.f1
+B_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	B.f2
+	.word	B.g
+C_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+String_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	String.length
+	.word	String.concat
+	.word	String.substr
+Bool_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+Int_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+IO_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	IO.out_string
+	.word	IO.out_int
+	.word	IO.in_string
+	.word	IO.in_int
+Object_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	-1
+Main_protObj:
+	.word	8
+	.word	9
+	.word	Main_dispTab
+	.word	-1
+A_protObj:
+	.word	7
+	.word	6
+	.word	A_dispTab
+	.word	-1
+B_protObj:
+	.word	6
+	.word	4
+	.word	B_dispTab
+	.word	-1
+C_protObj:
+	.word	5
+	.word	5
+	.word	C_dispTab
+	.word	-1
+String_protObj:
+	.word	4
+	.word	5
+	.word	String_dispTab
+	.word	-1
+Bool_protObj:
+	.word	3
+	.word	4
+	.word	Bool_dispTab
+	.word	-1
+Int_protObj:
+	.word	2
+	.word	4
+	.word	Int_dispTab
+	.word	-1
+IO_protObj:
+	.word	1
+	.word	3
+	.word	IO_dispTab
+	.word	-1
+Object_protObj:
+	.word	0
+	.word	3
+	.word	Object_dispTab
+	.globl	heap_start
+heap_start:
+	.word	0
+	.text
+	.globl	Main_init
+	.globl	Int_init
+	.globl	String_init
+	.globl	Bool_init
+	.globl	Main.main
+
+# end of generated code
